f8515fda3816d783d861de6c814d45b7e5b19151
Adhi Setiawan <adhisetiawan518@gmail.com>
2023-09-07 11:17:22 +0700

    adding output 3 vehicle 40 cust, and decode the output

---
+++ fleet_v3/results/hcvrp/hcvrp_v3_40_seed24610/decode.py
@@ -0,0 +1,26 @@
+import pickle
+
+# Specify the path to your pickle file
+pickle_file_path = 'hcvrp_v3_40_seed24610-hcvrp40_rollout_20230809T064053_epoch-49-greedy-t1-0-1280.pkl'
+
+# Load data from the pickle file
+with open(pickle_file_path, 'rb') as f:
+    loaded_data = pickle.load(f)
+
+print(f"Total instance: {len(loaded_data[0])}")
+print("")
+print(f"Get 1st instance: {loaded_data[0][0]}")
+print("")
+print(f"Cost: {loaded_data[0][0][0]}")
+print("")
+print(f"Get tour in 1st instance: {loaded_data[0][0][1]}")
+print("")
+print(f"Vehicle tour in each customer: {loaded_data[0][0][2]}")
+print("")
+print(f"Duration: {loaded_data[0][0][3]}")
+print(f"Length tour in 1st instance: {len(loaded_data[0][0][1])}")
+print(f"Max number tour in 1st instance: {max(loaded_data[0][0][1])}")
+# import pprint
+
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(loaded_data)
